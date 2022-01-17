b19fc63f7ac51298f5a9af4672b6096520226cf1
goryanski <igorok208@gmail.com>
2022-01-17 15:06:02 +0200

    page not-found is done, page home is ready to get data

--- Client/PhotoMania/src/app/app-routing.module.ts
+++ Client/PhotoMania/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    // if empty path -> go to home (so, home is default page)
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
